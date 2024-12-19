3de8b7166a9a771ab40f3bc40406ab8c67aa58e6
Ofer Mendelevitch <ofer@vectara.com>
2024-12-18 18:24:00 -0600

    added SDK example notebook (#16)

--- notebooks/vectaraClient.py
+++ notebooks/vectaraClient.py
@@ -116,3 +116,42 @@ class VectaraClient():
                 break
     
         return docs
+
+    def index_document(self, document):
+        """
+        Index a document (by uploading it to the Vectara corpus) from the document dictionary
+        """
+        api_endpoint = f"https://api.vectara.io/v1/index"
+        request = {
+            'customer_id': self.customer_id,
+            'corpus_id': self.corpus_id,
+            'document': document,
+        }
+        post_headers = { 
+            'x-api-key': self.api_key,
+            'customer-id': self.customer_id,
+        }
+        try:
+            data = json.dumps(request)
+        except Exception as e:
+            print(f"Can't serialize request {request}, skipping, e={e}")   
+            return False
+        
+        try:
+            response = requests.post(api_endpoint, data=data, verify=True, headers=post_headers)
+        except Exception as e:
+            print(f"Exception {e} while indexing document {document['documentId']}")
+            return False
+        if response.status_code != 200:
+            print("REST upload failed with code %d, reason %s, text %s",
+                  response.status_code,
+                  response.reason,
+                  response.text)
+            return False
+        result = response.json()
+        if "status" in result and result["status"] and "OK" in result["status"]["code"]:
+            return True
+        else:
+            return False
+
+
